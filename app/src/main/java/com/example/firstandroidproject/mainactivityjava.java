604c808a0b1018a0365448e187f5485182ee4262
Codebymahnoor786 <mughalmnoor78@gmail.com>
2025-09-02 15:31:26 +0500

    First java Hello World commit

--- app/src/main/java/com/example/firstandroidproject/mainactivityjava.java
+++ app/src/main/java/com/example/firstandroidproject/mainactivityjava.java
@@ -1,4 +1,4 @@
-package com.example.firstandroidproject;   // <-- apna exact package name lagana
+package com.example.firstandroidproject;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,6 +8,6 @@ public class mainactivityjava extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);  // yahan wahi layout chalega jo Kotlin wali use kar rahi thi
+        setContentView(R.layout.activity_main_java);
     }
 }
